8a6735a561dce072c43626f5d839f914235fbd4e
mohamedanwarr <mohamedanwarr@gmial.com>
2023-02-08 22:25:18 +0200

    doneeee

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tawssel',
+      title: 'sadasdasdasd',
       theme: ThemeData(
 
         primarySwatch: Colors.purple,
