fdb75e6cd5ddb20d2b5372781744f316efe3f0d0
mido4315 <mohmed.mahmoud3@gmail.com>
2023-02-17 07:56:32 +0200

    add navigator to signORlog screen

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,23 +1,42 @@
 import 'package:go_router/go_router.dart';
 import 'package:tawssel/features/order%20details/Presentation/Views/order_details.dart';
 
+import '../../features/Profile/Presentation/Views/Profile_Screen.dart';
+import '../../features/SignLog/presentation/views/LogIn.dart';
+import '../../features/SignLog/presentation/views/SignIn.dart';
 import '../../features/SignLog/presentation/views/sginORlog.dart';
 import '../../features/SplashScreen/SplashScreen.dart';
 import '../../features/add order screen/presentation/views/Order Page.dart';
 import '../../features/home/presentation/views/home_view.dart';
-
+import '../../features/rest password/presentation/views/homescreen.dart';
 
 abstract class AppRouter {
   static const kHomePage = '/HomeView';
   static const kOrderDetailsPage = '/OrderDetailsPage';
   static const kAddOrderPage = '/AddOrderPage';
   static const kSignOrLogPage = '/SignOrLogPage';
+  static const kProfilePage = '/ProfilePage';
+  static const kRestPasswordPage = '/RestPasswordPage';
+  static const kSignInPage = '/SignInPage';
+  static const kLogInPage = '/LogInPage';
 
   static final router = GoRouter(
     routes: [
       GoRoute(
         path: '/',
         builder: (context, state) => const SplashView(),
+      ),
+      GoRoute(
+        path: kSignOrLogPage,
+        builder: (context, state) => SignOrLog(),
+      ),
+      GoRoute(
+        path: kSignInPage,
+        builder: (context, state) => SignIn(),
+      ),
+      GoRoute(
+        path: kLogInPage,
+        builder: (context, state) => const LogIn(),
       ),
       GoRoute(
         path: kHomePage,
@@ -32,9 +51,14 @@ abstract class AppRouter {
         builder: (context, state) => const NewOrder(),
       ),
       GoRoute(
-        path: kSignOrLogPage,
-        builder: (context, state) => SignOrLog(),
+        path: kProfilePage,
+        builder: (context, state) => const ProfileScreen(),
       ),
+      GoRoute(
+        path: kRestPasswordPage,
+        builder: (context, state) => const Homescreen(),
+      ),
+
     ],
   );
 }
